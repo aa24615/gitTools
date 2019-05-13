41a82a5018620ecff505367237914c09b86b9e91
读心印 <zyan@duxinyindeMacBook-Pro.local>
2019-05-13 08:51:08 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# gitTools
-git自动批量提交,防止忘记提交,代码丢失
+# git自动批量提交
+每个程序员都不可能开发一个项目,每天下班或者意外可能丢失代码
+为了能够让代码丢失率更低,使用git自动批量提交,也许能帮助到您!
+
+## 下载/克隆到你的本地
+```
+$ git clone git@github.com:aa24615/gitTools.git
+
+```
+将脚本放置到任意目录,例如 /www/gitTools/
+
+## linux/mac平台使用方法
+
+####请规范放置你的网站项目
+/www 作为根目录 多个站点请放在 www/ 下
+例如
+```
+/www/web1/
+/www/web2/
+
+```
+
+###自动提交请将本脚本添加到 crontab 中 最少每天执行一次
+```
+$ crontab -e
+ 
+0 8 * * * sh /www/gitTools/push.sh #每天8点提交一次
+0 */1 * * * sh /www/gitTools/push.sh #每隔1小时提交一次
+
+
+```
+
+##win平台使用方法
+整理中...
+
+## 作者博客
+
+- [http://blog.php127.com/](http://blog.php127.com/) 
---
+++ push.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#################################
+##########git自动批量提交##########
+#################################
+
+dates=`date "+%Y-%m-%d"` #日期
+times=`date "+%H:%M:%S"` #时间
+logs=/www/gitTools/logs/$dates.log #日志
+echo "==========="$times"===========" >> $logs
+
+root="/www" #程序根目录
+list=$(ls -F $root | grep '/$') #获取网站列表
+for dirname in $list
+do
+    path=$root"/"$dirname #网站绝对路径;
+    cd $path #进入网站目录
+    isGit=$(ls -a|grep -x .git | wc -l) #判断是否有仓库
+    if [ $isGit -gt 0 ];then
+        isAdd=$(git status | grep "git add" | wc -l) #判断是否需要提交
+        if [ $isAdd -gt 0 ];then
+            git add .
+            git commit -m 自动提交_$dates_$times
+            git push origin master
+            echo "+提交完成" $path >> $logs
+        else
+            echo "-没有更改" $path >> $logs
+        fi
+    else
+        echo "x没有仓库" $path >> $logs
+    fi
+    
+done
+
+echo "">>$logs #隔行
+
+
